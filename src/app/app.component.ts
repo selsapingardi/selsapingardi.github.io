37c6ff1233612cd98e70e0752387331bf387da51
selsapingardi <selsa2711@gmail.com>
2024-04-13 22:29:33 +0700

    add base files and configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatOption } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EducationComponent } from './components/education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatButtonModule,
+    MatDivider,
+    MatIcon,
+    MatCheckbox,
+    MatFormField,
+    MatSelectModule,
+    MatLabel,
+    MatOption,
+    AboutMeComponent,
+    ExperiencesComponent,
+    ProjectsComponent,
+    EducationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  darkTheme = signal(false);
+
+  constructor() {
+    effect(() => {
+      document.body.classList.toggle('dark', this.darkTheme());
+    });
+  }
 }
