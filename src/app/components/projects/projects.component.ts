5976b8b826f97bf5cd969e733592809f6cc19079
selsapingardi <selsa2711@gmail.com>
2024-04-28 16:47:46 +0700

    add education section and refine other sections

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -17,11 +17,5 @@ export class ProjectsComponent {
       title: 'Integrating Google Maps in Angular 17',
       link: 'https://medium.com/@selsa-pingardi/integrating-google-maps-in-angular-17-66487ed2238c',
     },
-    {
-      year: 2021,
-      platform: 'itch.io',
-      title: 'Pet Treck: A Fitness Tracking Website & Mobile Application',
-      link: 'https://selsapingardi.itch.io/pet-treck',
-    },
   ];
 }
